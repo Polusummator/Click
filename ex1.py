5193e22666ecdbecc987c81739de581eba1446e9
Polusummator <49287742+Polusummator@users.noreply.github.com>
2019-04-27 21:54:34 +0300

    Click_Game --version 1.2.0
    +Добавлена отправка счёта разработчику
    +Добавлен ввод имени

--- ex1.py
+++ ex1.py
@@ -3,6 +3,7 @@
 from tkinter import *
 import os.path
 from cryptography.fernet import Fernet
+import smtplib
 
 
 def drk():
@@ -47,17 +48,52 @@ def on_close(event):
     text = crypto(score)
     f.write(text)
     f.close()
+    email()
     root.destroy()
 
 
+def email():
+    score = toFixed(float(coin.cget('text')))
+    log = open(r'C:\Click\Login.txt', 'r')
+    l = log.readline()
+    l = l.strip()
+    smtpObj = smtplib.SMTP('smtp.mail.ru', 587)
+    smtpObj.starttls()
+    smtpObj.login('user_in@mail.ru', '2580aa2580aa')
+    smtpObj.sendmail('user_in@mail.ru', 'srez2004@mail.ru', score + ' ' + l)
+    smtpObj.quit()
+    log.close()
+
+
+def ok1(event):
+    log = open(r'C:\Click\Login.txt', 'w+')
+    login = vvod.get()
+    log.write(login)
+    log.close()
+    root2.destroy()
+
+
 def main():
+    global vvod
     global coin
     global root
+    global root2
     z = True
     if not file():
         path = r'C:\Click'
         if not drk():
             os.mkdir(path)
+            root2 = Tk()
+            root2.title('Login')
+            root2.geometry('300x150+630+300')
+            label = Label(root2, text='Введите своё имя', font='Fixedays 14', fg='black')
+            vvod = Entry(root2, width=15, font='Fixedays 16', fg='black', bd=2)
+            ok = Button(root2, width=3, height=1, font='Fixedays 14', fg='white', bg='green', text='OK')
+            label.pack()
+            vvod.place(x=25, y=60)
+            ok.place(x=240, y=55)
+            ok.bind('<Button-1>', ok1)
+            root2.mainloop()
         z = False
         f = open(r'C:\Click\Click.txt', 'wb+')
     else:
@@ -78,6 +114,7 @@ def main():
     root.geometry('300x350')
     lab = Label(root, text='Your score:', font='Fixedays 12', fg='AntiqueWhite4')
     coin = Label(root, text=str(toFixed(score)), font='Fixedays 17', fg='black')
+    email()
     close = Button(root, text='Close & Save', font='Fixedays 12', fg='white', width=10, height=1, bg='brown')
     cnopka = PhotoImage(file="but.png")
     but = Label(root, image=cnopka)
